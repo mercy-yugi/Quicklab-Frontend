fe64362919f7727e96008156f56c033b54d5e723
NakimuliSusan <nakimulimarysusan@gmail.com>
2022-10-09 11:44:35 +0300

    moving frontend into new repository

---
+++ quick-lab/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { BrowserRouter as Router, Route , Routes } from "react-router-dom";
+import SignupForm from './signup';
+import LoginForm from './login';
+
+
+
+function App() {
+  return (
+    <div>
+  <Router>
+  <Routes>
+        <Route path="/" element={<SignupForm/>}/>
+        <Route path="/login" element={<LoginForm/>}/>
+      
+  </Routes>  
+
+ </Router>
+    {/* <SignupForm/> */}
+    {/* <LoginForm/> */}
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ quick-lab/src/Footer.js
@@ -0,0 +1,11 @@
+import  react from 'react';
+
+const Foooter = () => {
+
+   return(
+      <div className="container">
+         <p>&copy;Cheche 2022</p>
+      </div>
+   )
+};
+export default Foooter;
