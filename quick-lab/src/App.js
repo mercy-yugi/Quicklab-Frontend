f5367fddd54d70e0ab68cf84fa1a19a681602b4f
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-10-10 13:43:47 +0300

    added and styled available and recent practicals in the dashboard

--- quick-lab/src/App.js
+++ quick-lab/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route , Routes } from "react-router-dom";
 import SignupForm from './signup';
 import LoginForm from './login';
+import Practicals from './dashboard/Practicals';
 
 
 
