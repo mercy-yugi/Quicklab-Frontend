371c6217c9126d1ea5ea567b0913c81cfc652c11
NakimuliSusan <nakimulimarysusan@gmail.com>
2022-10-17 17:37:34 +0300

    working on lab interface

--- quick-lab/src/labinterface/Interface.js
+++ quick-lab/src/labinterface/Interface.js
@@ -136,7 +136,7 @@ export default function PersistentDrawerLeft() {
         </List>
         <Divider />
         <List>
-        <Typography className='instructions'>Tools and Equipment</Typography>
+        {/* <Typography className='instructions'>Tools and Equipment</Typography> */}
           {/* {['All mail', 'Trash', 'Spam'].map((text, index) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
@@ -158,7 +158,58 @@ export default function PersistentDrawerLeft() {
           
         </Typography>
       </Main>
+      <Drawer className='drawer'
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+            <Typography className='instructions'>Lab Equipment</Typography>
+            <Typography paragraph>
+               
+        </Typography>
+          {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))} */}
+        </List>
+        <Divider />
+        <List>
+        {/* <Typography className='instructions'>Tools and Equipment</Typography> */}
+          {/* {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))} */}
+        </List>
+      </Drawer>
       
     </Box>
+    
   );
 }
