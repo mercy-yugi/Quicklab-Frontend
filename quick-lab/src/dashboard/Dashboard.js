4f767a17729bdcfee9060af71733f8cb3e0e8585
mercy-yugi <mercyyugi@gmail.com>
2022-10-19 17:22:05 +0300

    adding timer functionality

--- quick-lab/src/dashboard/Dashboard.js
+++ quick-lab/src/dashboard/Dashboard.js
@@ -2,11 +2,13 @@ import react from 'react';
 import logo from '../Images/Quicklab_logo.png';
 import '../dashboard/dashboard.css';
 import Sidebar from './Sidebar';
+import Foooter from '../Footer';
 
 const Dashboard = () => {
     return (
         <div>
            <Sidebar/>
+           <Foooter/>
         </div>
     )
 
