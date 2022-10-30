adf35c24a2e85408091044362f25cf508e5beba7
NakimuliSusan <nakimulimarysusan@gmail.com>
2022-10-30 18:14:50 +0300

    merged popper into interface

--- quick-lab/src/App.js
+++ quick-lab/src/App.js
@@ -3,18 +3,17 @@ import { BrowserRouter as Router, Route , Routes } from "react-router-dom";
 import SignupForm from './SignUp/signup';
 import LoginForm from './Login/login';
 import Dashboard from './dashboard/Dashboard';
-import Practicals from './dashboard/Practicals';
 import Interface from './LabInterface/Interface';
-import Test from './Test';
+
 
 function App() {
   return (
     <div>
-
-      {/* <Test/> */}
   <Router>
   <Routes>
-        <Route path="/login" element={<LoginForm/>}/>
+      
+       <Route path="/" element={<SignupForm/>}/>
+       <Route path="/login" element={<LoginForm/>}/>
         <Route path="/home" element={<Dashboard/>}/>
         <Route path="/canvas" element={<Interface/>}/>
 
